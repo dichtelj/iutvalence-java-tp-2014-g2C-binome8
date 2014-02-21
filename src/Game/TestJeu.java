0b5f00a71876cb02c3c11d2eb8dfef4d58457b77
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-21 23:31:33 +0100

    Add TODO (TestJeu.java)

--- src/Game/TestJeu.java
+++ src/Game/TestJeu.java
@@ -1,9 +1,11 @@
+/* TODO Convention java : les packages sont en minuscules */
 package Game;
 
 public class TestJeu {
 
 	public static void main(String[] args) 
 	{
+        /* TODO D'où sortent ces variables ? */
 		for (i=1;i<3;i++){
 			for(j=1;j<3;j++){
 				System.out.println("Bille = "+ i +" "+ j);
