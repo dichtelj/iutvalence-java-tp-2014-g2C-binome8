4a02a8198ddbb62f7f372a7f86b5f4fde7a50b4f
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-27 00:48:56 +0100

    Add TODO (ErreurCase.java)

--- src/game/ErreurCase.java
+++ src/game/ErreurCase.java
@@ -1,6 +1,8 @@
 package game;
 
+/* TODO Javadoc. */
+/* TODO Nom de package pertinent. */
 public class ErreurCase extends Exception 
 {
-
+    /* TODO Bonne pratique : ne pas faire d'exception sans parametre. */
 }
