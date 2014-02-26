86da384432f0ee71acdeb3185f08dd4b5288ad7b
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-27 00:48:46 +0100

    Add TODO (Bille.java)

--- src/game/Bille.java
+++ src/game/Bille.java
@@ -1,27 +1,29 @@
 package game;
-
+/* TODO Javadoc. */
+/* TODO Nom de package pertinent. */
 public class Bille {
 
-	private int nbLigne;
-	private int nbColonne;
-	private Joueur joueur;
-	
-	public Bille(int posLigne, int posColonne, Joueur player) 
-	{
-		this.nbLigne = posLigne;
-		this.nbColonne = posColonne;
-		this.joueur = player;
-	}
-	public int getLigne() 
-	{
-		return this.nbLigne;
-	}
-	public int getColonne() 
-	{
-		return this.nbColonne;
-	}	
-	public Joueur getJoueur() 
-	{
-		return this.joueur;
-	}
+    private int    nbLigne;
+    private int    nbColonne;
+    private Joueur joueur;
+
+    public Bille(int posLigne, int posColonne, Joueur player) {
+        this.nbLigne = posLigne;
+        this.nbColonne = posColonne;
+        this.joueur = player;
+    }
+    /* TODO Franglais. */
+    public int getLigne() {
+        return this.nbLigne;
+    }
+
+    /* TODO Franglais. */
+    public int getColonne() {
+        return this.nbColonne;
+    }
+
+    /* TODO Franglais. */
+    public Joueur getJoueur() {
+        return this.joueur;
+    }
 }
