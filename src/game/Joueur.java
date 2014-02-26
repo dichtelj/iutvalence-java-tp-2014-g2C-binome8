5063ff026b571490e7e70affcc31dc3c3f21382f
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-27 00:49:10 +0100

    Add TODO (Joueur.java)

--- src/game/Joueur.java
+++ src/game/Joueur.java
@@ -1,32 +1,35 @@
 package game;
 
+/* TODO Javadoc. */
+/* TODO Nom de package pertinent. */
 public class Joueur {
-	public final String nom;
-	public final boolean couleurJoueur;
-	public final boolean IA;
-	
-	public static final boolean Blanc=true;
-	public static final boolean Noir=false;
-	
-	
-	public Joueur(String nom, boolean couleurJoueur, boolean iA) {
-		this.nom = nom;
-		this.couleurJoueur = couleurJoueur;
-		IA = iA;
-	}
-	
-	public String getName()
-	{
-		return this.nom;
-	}
-	
-	public boolean getColor()
-	{
-		return couleurJoueur;
-	}
-	public boolean getIA()
-	{
-		return IA;
-	}
-	
+    public final String  nom;
+    public final boolean couleurJoueur;
+    public final boolean IA;
+
+    public static final boolean Blanc = true;
+    public static final boolean Noir  = false;
+
+
+    public Joueur(String nom, boolean couleurJoueur, boolean iA) {
+        this.nom = nom;
+        this.couleurJoueur = couleurJoueur;
+        IA = iA;
+    }
+
+    /* TODO Franglais. */
+    public String getName() {
+        return this.nom;
+    }
+
+    /* TODO Franglais. */
+    public boolean getColor() {
+        return couleurJoueur;
+    }
+
+    /* TODO Franglais. */
+    public boolean getIA() {
+        return IA;
+    }
+
 }
