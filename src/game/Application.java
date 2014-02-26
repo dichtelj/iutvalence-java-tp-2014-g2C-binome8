71bb0022f3bb7753a1eef1b558c33f9e26964f1b
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-27 00:48:34 +0100

    Add TODO (Application.java)

--- src/game/Application.java
+++ src/game/Application.java
@@ -1,10 +1,12 @@
 package game;
+/* TODO Javadoc. */
+/* TODO Nom de package pertinent. */
 
 public class Application {
 
 	public static void main(String[] args) {
 		// Initialisation du jeu
-		
+
 		Jeu j = new Jeu();
 		j.jouer();
 	}
