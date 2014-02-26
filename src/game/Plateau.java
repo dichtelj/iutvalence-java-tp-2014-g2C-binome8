fc4d59c21e65e83bc07d581bdf2e18c736d475bb
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-27 00:49:22 +0100

    Add TODO (Plateau.java)

--- src/game/Plateau.java
+++ src/game/Plateau.java
@@ -1,23 +1,23 @@
 package game;
 
+/* TODO Javadoc. */
+/* TODO Nom de package pertinent. */
 public class Plateau {
-	public Bille[][] plat;
-	
-	public Plateau() 
-	{
-		this.plat = new Bille[9][9];
-		/* TODO Initialisation des cases. */
-		
-	}
-//	public boolean caseVide(int nbLigne, int nbColonne) 
-//	{
-//	try {
-//		return this.plat[nbLigne][nbColonne] == null;
-//	}
-//
-//	catch(ErreurCase e) {
-//		System.out.println("Erreur test case : coordonnees hors plateau");
-//		return false;
-//}
-//}
+    public Bille[][] plat;
+
+    public Plateau() {
+        this.plat = new Bille[9][9];
+        /* TODO Initialisation des cases. */
+    }
+    //	public boolean caseVide(int nbLigne, int nbColonne)
+    //	{
+    //	try {
+    //		return this.plat[nbLigne][nbColonne] == null;
+    //	}
+    //
+    //	catch(ErreurCase e) {
+    //		System.out.println("Erreur test case : coordonnees hors plateau");
+    //		return false;
+    //}
+    //}
 }
